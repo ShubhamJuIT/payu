5e3e550a140a5a31914bedc9c7e8fab1c7aa869c
Shubham Kumar <shubham.ekodemy@gmail.com>
2021-02-19 09:29:22 +0530

    Fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,13 +20,16 @@ class _MyAppState extends State<MyApp> {
   // Creating PayuMoneyFlutter Instance
   PayuMoneyFlutter payuMoneyFlutter = PayuMoneyFlutter();
 
+//fnuDx1|602ed0df6cea174709c27337|500.00|bucket-1|Shubham|shubham.mua@gmail.com|||||||||||99FzqrK2
   // Payment Details
-  String phone = "8318045008";
-  String email = "gmail@gmail.com";
-  String productName = "My Product Name";
-  String firstName = "Vaibhav";
-  String txnID = "223428947";
-  String amount = "1.0";
+  String phone = "7838821488";
+  String email = "shubham.mua@gmail.com";
+  String productName = "bucket-1";
+  String firstName = "Shubham";
+  String txnID = "602ed9b16cea172139bc7608";
+  String amount = "500.0";
+  String hash =
+      "526b9eeb5e835d44bdfd43d9e7d88d9b111ab7d40f1f1c5efbbd310a2c96ec229f660dea59374e97d003e2a724d0a769730ef19bfa1ab1ea65bb1cc5852af9a6";
 
   @override
   void initState() {
@@ -38,8 +41,8 @@ class _MyAppState extends State<MyApp> {
   // Function for setting up the payment details
   setupPayment() async {
     bool response = await payuMoneyFlutter.setupPaymentKeys(
-        merchantKey: "CA9nIRrB",
-        merchantID: "53HWzcRBbU",
+        merchantKey: "fnuDx1",
+        merchantID: "130033",
         isProduction: false,
         activityTitle: "App Title",
         disableExitConfirmation: false);
@@ -48,18 +51,18 @@ class _MyAppState extends State<MyApp> {
   // Function for start payment with given merchant id and merchant key
   Future<Map<String, dynamic>> startPayment() async {
     // Generating hash from php server
-    Response res =
-        await post("https://PayUMoneyServer.codedivinedivin.repl.co", body: {
-      "txnid": txnID,
-      "phone": phone,
-      "email": email,
-      "amount": amount,
-      "productinfo": productName,
-      "firstname": firstName,
-    });
-    var data = jsonDecode(res.body);
-    print(data);
-    String hash = data['params']['hash'];
+    // Response res =
+    //     await post("https://PayUMoneyServer.codedivinedivin.repl.co", body: {
+    //   "txnid": txnID,
+    //   "phone": phone,
+    //   "email": email,
+    //   "amount": amount,
+    //   "productinfo": productName,
+    //   "firstname": firstName,
+    // });
+    // var data = jsonDecode(res.body);
+    // print(data);
+    // String hash = data['params']['hash'];
     print(hash);
     var myResponse = await payuMoneyFlutter.startPayment(
         txnid: txnID,
--- test/payu_money_flutter_test.dart
+++ test/payu_money_flutter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:payu_money_flutter/payu_money_flutter.dart';
+import 'package:payu/payu_money_flutter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('payu_money_flutter');
